98112798f5fd3157c06e236dbad7c2fbdda57741
PixelToast <andre.lipke@gmail.com>
2014-08-18 12:22:28 -0400

    Update

--- rpi.c
+++ rpi.c
@@ -7,13 +7,14 @@
 //  onChange pin function
 //  PWM
 //  I2C
-//  blocking UART functions
 
-#include <stdlib.h>
+
+#define UARTfile "/dev/ttyAMA0"
+
 
 #include "lua.h"
 #include "lauxlib.h"
-
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/mman.h>
@@ -21,8 +22,8 @@
 #include <termios.h>
 #include <errno.h>
 #include <poll.h>
-
-int pimodel;
+#include <string.h>
+#include <sys/select.h>
 
 void settable(lua_State *L,char *name,void *func) {
 	lua_pushstring(L,name);
@@ -30,7 +31,7 @@ void settable(lua_State *L,char *name,void *func) {
 	lua_settable(L,-3);
 }
 
-////////////////////// GPIO pins //////////////////////
+//////////////////////////////////////////// GPIO pins ////////////////////////////////////////////
 
 volatile int *gpiomem=0;
 int mem=0;
@@ -49,7 +50,7 @@ static int lrpi_openpins(lua_State *L) {
 	gpiomem=mmap(NULL,4096,3,1,mem,0x20200000);
 	if (gpiomem==MAP_FAILED) {
 		gpiomem=0;
-		luaL_error(L,"mmap failed (errno=%i)",errno);
+		luaL_error(L,"mmap failed (%i)",strerror(errno));
 	}
 	return 0;
 }
@@ -59,13 +60,10 @@ int dirs[32]={0};
 void pin_setdir(int pin, int dir) {
 	if (dirs[pin]!=dir) {
 		dirs[pin]=dir;
+		dir=dir==1?1:0;
 		int idx=pin%10;
-		gpiomem[idx]=((gpiomem[(pin-idx)/10])&((7<<(idx*3))))|(dir<<(idx*3));
+		gpiomem[(pin-idx)/10]=((gpiomem[(pin-idx)/10])&((7<<(idx*3))))|(dir<<(idx*3));
 	}
-}
-
-int pin_valid[][]={
-	{}
 }
 
 void pin_isvalid(lua_State *L,int pin) {
@@ -82,8 +80,7 @@ static int lrpi_pin_newindex(lua_State *L) {
 		luaL_error(L,"value must be 1 or 0");
 	}
 	pin_setdir(pin,1);
-	printf("k=%i v=%i\n",10-val*3,1 << pin);
-	gpiomem[10-val*3]=1 << pin;
+	gpiomem[10-val*3]=1<<pin;
 	return 0;
 }
 
@@ -91,7 +88,7 @@ static int lrpi_pin_index(lua_State *L) {
 	int pin=luaL_checkinteger(L,2);
 	pin_isvalid(L,pin);
 	pin_setdir(pin,-1);
-	lua_pushinteger(L,(gpiomem[13] >> pin)&1);
+	lua_pushinteger(L,(gpiomem[13]>>pin)&1);
 	return 1;
 }
 
@@ -106,7 +103,7 @@ void pin_load(lua_State *L) {
 	lua_settable(L,-3);
 }
 
-////////////////////// UART //////////////////////
+//////////////////////////////////////////// UART ////////////////////////////////////////////
 
 int uartfs=-1;
 struct termios uartfs_opt;
@@ -133,18 +130,16 @@ static int lrpi_uart_open(lua_State *L) {
 	if (uartfs>0) {
 		close(uartfs);
 	}
-	uartfs=open("/dev/ttyAMA0",O_RDWR|O_NOCTTY|O_NDELAY);
+	uartfs=open(UARTfile,O_RDWR|O_NOCTTY|O_NDELAY);
 	if (uartfs<0) {
-		luaL_error(L,"error opening /dev/ttyAMA0");
+		luaL_error(L,"error opening %s (%s)",UARTfile,strerror(errno));
 	}
 	tcgetattr(uartfs,&uartfs_opt);
 	int baud=0;
 	if (lua_gettop(L)>0) {
 		int nbaud=luaL_checkinteger(L,1);
 		for (int i=0;validbaud[i][0]!=0;i++) {
-			printf("derrr %i %i\n",validbaud[i][0],validbaud[i][1]);
 			if (validbaud[i][0]==nbaud) {
-				printf("guuud\n");
 				baud=validbaud[i][1];
 				break;
 			}
@@ -164,7 +159,7 @@ static int lrpi_uart_open(lua_State *L) {
 	return 0;
 }
 
-static int lrpi_uart_asend(lua_State *L) {
+static int lrpi_uart_send(lua_State *L) {
 	if (uartfs<0) {
 		luaL_error(L,"UART not opened");
 	}
@@ -174,14 +169,10 @@ static int lrpi_uart_asend(lua_State *L) {
 			lua_pushboolean(L,0);
 			return 1;
 		}
-		luaL_error(L,"error sending errno=%i",errno);
+		luaL_error(L,"error sending (%s)",strerror(errno));
 	}
 	lua_pushboolean(L,1);
 	return 1;
-}
-
-static int lrpi_uart_send(lua_State *L) { // TODO
-	return 0;
 }
 
 static int lrpi_uart_areceive(lua_State *L) {
@@ -201,8 +192,20 @@ static int lrpi_uart_areceive(lua_State *L) {
 	return 1;
 }
 
-static int lrpi_uart_receive(lua_State *L) { // TODO
-	return 0;
+static int lrpi_uart_receive(lua_State *L) {
+	fd_set rfds;
+	FD_ZERO(&rfds);
+	FD_SET(uartfs,&rfds);
+	if (lua_gettop(L)>0) {
+		double timeout=luaL_checkinteger(L,1);
+		struct timeval tv;
+		tv.tv_sec=timeout;
+		tv.tv_usec=(int)(timeout*1000)%10000;
+		select(uartfs+1,&rfds,NULL,NULL,&tv);
+	} else {
+		select(uartfs+1,&rfds,NULL,NULL,NULL);
+	}
+	return lrpi_uart_areceive(L);
 }
 
 static int lrpi_uart_getfd(lua_State *L) {
@@ -214,7 +217,6 @@ void uart_load(lua_State *L) {
 	lua_pushstring(L,"uart");
 	lua_newtable(L);
 	settable(L,"open",lrpi_uart_open);
-	settable(L,"asend",lrpi_uart_asend);
 	settable(L,"send",lrpi_uart_send);
 	settable(L,"receive",lrpi_uart_receive);
 	settable(L,"areceive",lrpi_uart_areceive);
@@ -222,10 +224,10 @@ void uart_load(lua_State *L) {
 	lua_settable(L,-3);
 }
 
-////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 
 static int lrpi_sleep(lua_State *L) {
-	poll(NULL,0,luaL_checkinteger(L,1)*1000);
+	poll(NULL,0,luaL_checknumber(L,1)*1000);
 	return 0;
 }
 
